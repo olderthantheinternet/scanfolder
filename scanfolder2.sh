fc0ae1b9a942d17d539efcbe56d61f2c57c157e9
hthighwaymonk <49250826+hthighwaymonk@users.noreply.github.com>
2020-11-27 21:46:40 -0800

    Update scanfolder2.sh

--- scanfolder2.sh
+++ scanfolder2.sh
@@ -45,7 +45,8 @@ get_files ()
   declare -a file_list
   find "$DIR" -maxdepth "${depth}" -type f | while read filename; do
     FILESIZE=$(stat -c "%s" "$filename")  # get file size
-    file_list+=( "${filename}|${FILESIZE}" )
+    echo "$filename|$FILESIZE"
+    file_list+=("$filename|$FILESIZE")
   done
   printf '%s\n' "${file_list[@]}"
   exit;
