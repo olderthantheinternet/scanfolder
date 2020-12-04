8a5dea92d584d55d29ae8d07dbeafe597eb5d351
hthighwaymonk <49250826+hthighwaymonk@users.noreply.github.com>
2020-12-03 18:34:29 -0800

    Update diff_copy.sh

--- diff_copy.sh
+++ diff_copy.sh
@@ -11,50 +11,15 @@ while getopts a:b:c:d:e:p: option; do
      esac
 done
 
-get_files ()
+send_to_rclone ()
 {
-  case $TRIGGER in
-          movie)
-                  depth=2
-                  ;;
-          tv|television|series)
-                  depth=3
-                  ;;
-          '')
-                  echo "Media type parameter is empty"
-                  exit;
-                  ;;
-          *)
-                  echo "Media type specified unknown"
-                  exit;
-                  ;;
-  esac
   IFS=$'\n' 
-  filelist=($(rclone lsf --files-only --absolute --max-depth "$depth" --format pt --separator "|" "$RCLONEMOUNT:$ZDTD/$SOURCE_FOLDER"))
-  unset IFS
-  file_list=()
-  for i in "${filelist[@]}"
+  for i in "${1[@]}"
   do
-     file_list+=("${CONTAINER_FOLDER}${SOURCE_FOLDER}${i}")
+     rclone copy "$YOURRCLONE/$i" "ZDRCLONE" -vP --stats=10s --drive-use-trash=false --drive-server-side-across-configs=true \
+     --transfers 16 --checkers=16 --use-mmap  --max-backlog=2000000 --tpslimit 4 --tpslimit-burst 32
   done
-}
-
-get_db_items ()
-{ 
-         cmd="select p.file,p.created_at from media_items m inner join media_parts p on m.id=p.media_item_id WHERE p.file LIKE '%$SOURCE_FOLDER/%'"
-         if [ ! -z "$PLEXDB" ]
-         then
-             plex="${PLEXDB}com.plexapp.plugins.library.db"
-         else
-             plex="/opt/plex/Library/Application Support/Plex Media Server/Plug-in Support/Databases/com.plexapp.plugins.library.db"
-         fi
-         db_list=()
-         IFS=$'\n'
-         fqry=(`sqlite3 "$plex" "$cmd"`)
-         unset IFS
-         for f in "${fqry[@]}"; do
-           db_list+=("${f}")
-         done
+  unset IFS  
 }
 
 process_diff ()
@@ -109,33 +74,12 @@ WHERE md2.library_section_id = '$SECID' AND p2.file NOT LIKE '%$YOURMEDIA%' \
 }
 
 process_diff
-printf '%s\n' "${db_list[@]}"
-exit;
+#printf '%s\n' "${db_list[@]}"
 IFS=$'\n'
-mapfile -t missing_files < <( comm -13 <(printf '%s\n' "${db_list[@]}" | LC_ALL=C sort) <(printf '%s\n' "${file_list[@]}" | LC_ALL=C sort) )
-unset IFS
-declare -a farray
-for i in "${missing_files[@]}"; 
-do
-  f=("$(cut -d '|' -f1 <<< "$i")");
-  f=${f//[$'\t\r\n']}
-  farray+=("$(dirname "${f}")")
-done
-IFS=$'\n'
-readarray -t uniq < <(printf '%s\n' "${farray[@]}" | sort -u)
+readarray -t uniq < <(printf '%s\n' "${db_list[@]}" | sort -u)
 unset IFS
 c=1
 for i2 in "${uniq[@]}"; 
 do 
-  g=${i2//[$'\t\r\n']}
-  if [ ! -z "$g" ]; then
-     if [ "${g}" != "${CONTAINER_FOLDER}${SOURCE_FOLDER}" ]; then
-        autoscan_check
-        if [ "$check" -eq "0" ]; then
-           process_autoscan "${g}";
-        fi
-     fi
-     c=$[$c +1]
-  fi
+  send_to_rclone "${i2}"
 done
-echo "${c} files processed"
