6b744a4122c757a7f9932489012478ad136a1e50
hthighwaymonk <49250826+hthighwaymonk@users.noreply.github.com>
2020-12-04 21:36:27 -0800

    Update scanfolder.sh
    ADDED a check to not pass values with only a "." to autoscan

--- scanfolder.sh
+++ scanfolder.sh
@@ -152,8 +152,7 @@ autoscan_check ()
          check=0
          FOO="$(echo -e "${g}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
          FOO=${#FOO}  
-         echo "$FOO"
-         if [ "`sqlite3 "$scan" "$sql"`" != "0" ] && [ $FOO > 2 ]
+        if [ "`sqlite3 "$scan" "$sql"`" != "0" ] && [ $FOO > 2 ]
          then
             check=1
          fi
