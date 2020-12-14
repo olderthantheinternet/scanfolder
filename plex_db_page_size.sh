6735b54d9f197fafc0f314612ad1d6a3e7e0b54c
hthighwaymonk <49250826+hthighwaymonk@users.noreply.github.com>
2020-12-14 11:20:24 -0800

    Update plex_db_page_size.sh

--- plex_db_page_size.sh
+++ plex_db_page_size.sh
@@ -2,27 +2,21 @@
 plexdb="/opt/plex/Library/Application Support/Plex Media Server/Plug-in Support/Databases/com.plexapp.plugins.library.db"
 plexdocker="plex"
 
-N=(`sqlite3 "$plexdb" "pragma page_size"`)
-if [ "$N" -eq "32768" ]; then 
-  echo "page_size is already set to $N" 
-  #exit; 
+N1=(`sqlite3 "$plexdb" "pragma page_size"`)
+if [ "$N1" -eq "32768" ]; then 
+  echo "page_size is already set to $N1" 
+  exit; 
 fi
 
-#docker stop "${plexdocker}"
-plexdb=$(dirname "$plexdb")
-cd "$plexdb/";
-exit;
-cp com.plexapp.plugins.library.db com.plexapp.plugins.library.db.original
-sqlite3 com.plexapp.plugins.library.db "DROP index 'index_title_sort_naturalsort'"
-sqlite3 com.plexapp.plugins.library.db "DELETE from schema_migrations where version='20180501000000'"
-sqlite3 com.plexapp.plugins.library.db .dump > dump.sql
-rm com.plexapp.plugins.library.db
-sqlite3 com.plexapp.plugins.library.db
-#will go into the sqlite console
-pragma page_size = 32768;
-vacuum;
-# ctrl+d to quit console
-# check page_size changed
-sqlite3 com.plexapp.plugins.library.db "pragma page_size;"
-sqlite3 com.plexapp.plugins.library.db < dump.sql
+docker stop "${plexdocker}"
+cp "$plexdb" "$plexdb.original"
+N2=(`sqlite3 "$plexdb" "DROP index 'index_title_sort_naturalsort'"`)
+N3=(`sqlite3 "$plexdb" "DELETE from schema_migrations where version='20180501000000'"`)
+sqlite3 "$plexdb" .dump > /tmp/dump.sql
+rm "$plexdb"
+N4=(`sqlite3 "$plexdb" "pragma page_size=32768"`)
+N5=(`sqlite3 "$plexdb" "vacuum"`)
+N6=(`sqlite3 "$plexdb" "pragma page_size"`)
+echo "$N6"
+sqlite3 "$plexdb" < /tmp/dump.sql
 docker start "${plexdocker}"
