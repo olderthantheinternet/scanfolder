12d40f221c84744eef2d539852ad637bb0470448
hthighwaymonk <49250826+hthighwaymonk@users.noreply.github.com>
2020-12-14 10:58:50 -0800

    Update plex_db_page_size.sh

--- plex_db_page_size.sh
+++ plex_db_page_size.sh
@@ -7,18 +7,17 @@ cmd="pragma page_size"
 #IFS=$'\n'
 fqry=(`sqlite3 "$plexdb" "$cmd"`)
 #unset IFS
-if [ "$fqry" -eq "$needed" ]; then
-   echo "page_size is already set to ${fqry}"
-   exit;
-fi
+if [ "$fqry" -eq "$needed" ]; then echo "page_size is already set to ${fqry}" exit; fi
+
 #docker stop "${plexdocker}"
 plexdb=$(dirname "$plexdb")
 echo "$plexdb"
 
 echo "page size needs changing as it's currently set to ${fqry}"
-exit;
+
 
 cd "${plexdb}"
+exit;
 cp com.plexapp.plugins.library.db com.plexapp.plugins.library.db.original
 sqlite3 com.plexapp.plugins.library.db "DROP index 'index_title_sort_naturalsort'"
 sqlite3 com.plexapp.plugins.library.db "DELETE from schema_migrations where version='20180501000000'"
